9349898a283d3b2d6eb7cfcdd8aba493e6d4b1ce
atm <atm@WORKSTATION_MAB>
2022-10-31 16:06:37 +0100

    first commit

--- dist/index.js
+++ dist/index.js
@@ -54450,7 +54450,7 @@ function delay(time) {
  }
 
  function setupcromium() {
-    exec('cd /home/runner/work/_actions/TIMEmSYSTEM/Screenshot-Taker && ls',
+    exec('cd /home/runner/work/_actions/TIMEmSYSTEM/Screenshot-Taker/master && ls',
     function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
