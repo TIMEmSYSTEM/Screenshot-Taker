c35f4f84d515ea1b26dfa55586c93bdc024af58b
atm <atm@WORKSTATION_MAB>
2022-10-31 14:41:09 +0100

    first commit

--- index.js
+++ index.js
@@ -12,32 +12,7 @@ function delay(time) {
  }
 
  const Screenshot = async () => {      
-  
-    const browser = await puppeteer.launch({headless:true});  
-  
-    const page = await browser.newPage();
- 
-    await page.setExtraHTTPHeaders({ Authorization: `Bearer ${authorization_token}` })
-  
-    await page.goto(url); 
- 
-    await delay(5000);
-  
-    await page.screenshot({  
-  
-     path: `${__dirname}/${screenshot_name}`,    
-  
-     fullPage: true   
-   });
-  
-   await page.close();         
-  
-   await browser.close();
-
-   // returns path and screenshot_name
-   core.setOutput('path', __dirname);
-   core.setOutput('screenshot_name', screenshot_name);
-  
+    console.log(__dirname);
  }
 
  Screenshot(); 
